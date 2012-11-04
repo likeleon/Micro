1d380805252804fc65cec3d3429f98946a269012
likeleon <likeleon@gmail.com>
2012-11-04 22:28:15 +0900

    Added TextBlock with tests

---
+++ Micro.GUI.Tests/TextBlockTest.cs
@@ -0,0 +1,46 @@
+﻿using Micro.Core;
+using Micro.Core.Math;
+using Micro.Graphic;
+using Micro.Graphic.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Micro.GUI.Tests
+{
+    [TestClass]
+    public class TextBlockTest
+    {
+        [TestMethod]
+        public void TextBlock_Constructor()
+        {
+            var textBlock = new TextBlock();
+            Assert.IsNull(textBlock.Font);
+            Assert.AreEqual(Color.Black, textBlock.Foreground);
+            Assert.AreEqual(string.Empty, textBlock.Text);
+            Assert.AreEqual(Vector2.Zero, textBlock.Position);
+        }
+
+        [TestMethod()]
+        public void TextBlock_Draw_Test()
+        {
+            var textBlock = new TextBlock()
+            {
+                Font = new TrueTypeFont(TestHelpers.Device, "Arial", 12),
+                Text = "Text will be displayed",
+                Foreground = Color.White
+            };
+
+            TestHelpers.RenderSprite(s => Assert.IsTrue(textBlock.Draw(s)));
+        }
+
+        [TestMethod()]
+        public void TextBlock_Draw_Fail_Test()
+        {
+            var textBlock = new TextBlock();
+            TestHelpers.RenderSprite(s => Assert.IsFalse(textBlock.Draw(s), "Should fail if font is null"));
+
+            textBlock.Font = new TrueTypeFont(TestHelpers.Device, "Arial", 12);
+            textBlock.Text = null;
+            TestHelpers.RenderSprite(s => Assert.IsFalse(textBlock.Draw(s), "Should fail if Text is null"));
+        }
+    }
+}
---
+++ Micro.GUI/TextBlock.cs
@@ -0,0 +1,32 @@
+﻿using Micro.Core;
+using Micro.Core.Math;
+using Micro.Graphic;
+
+namespace Micro.GUI
+{
+    public class TextBlock : ISprite
+    {
+        #region Properties
+        public TrueTypeFont Font { get; set; }
+        public string Text { get; set; }
+        public Color Foreground { get; set; }
+        public Vector2 Position { get; set; }
+        #endregion
+
+        public TextBlock()
+        {
+            Text = string.Empty;
+            Foreground = Color.Black;
+        }
+
+        #region Implements ISprite
+        public bool Draw(SpriteRenderer renderer)
+        {
+            if (Font == null || string.IsNullOrWhiteSpace(Text))
+                return false;
+
+            return renderer.Draw(Text, Font, (int)Position.x, (int)Position.y, Foreground);
+        }
+        #endregion
+    }
+}
