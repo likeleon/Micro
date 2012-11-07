f710b7da5cfad8bc6f04072de89e8edf18e0e6df
likeleon <likeleon@gmail.com>
2012-11-07 23:16:55 +0900

    Add Elysium support

--- Micro.Editor/Shell.xaml.cs
+++ Micro.Editor/Shell.xaml.cs
@@ -1,14 +1,16 @@
 ﻿using System.ComponentModel.Composition;
 using System.Windows;
+using Elysium.Theme;
 
 namespace Micro.Editor
 {
     [Export]
-    public partial class Shell : Window
+    public partial class Shell
     {
         public Shell()
         {
             InitializeComponent();
+            ThemeManager.Instance.Dark(ThemeManager.Instance.Accent);
         }
     }
 }
