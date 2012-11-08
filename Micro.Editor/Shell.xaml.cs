6f69a5cc3d6dddb3d6aa73894a24dbe5934cc217
likeleon <likeleon@gmail.com>
2012-11-09 00:18:13 +0900

    Random accent color on startup

--- Micro.Editor/Shell.xaml.cs
+++ Micro.Editor/Shell.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Media;
 using Elysium;
-using Elysium.Notifications;
 
 namespace Micro.Editor
 {
@@ -12,7 +14,15 @@ namespace Micro.Editor
         public Shell()
         {
             InitializeComponent();
-            Application.Current.Apply(Theme.Dark, AccentBrushes.Blue, Brushes.White);
+            Application.Current.Apply(Theme.Dark, GetRandomAccentBrush(), Brushes.White);
+        }
+
+        private SolidColorBrush GetRandomAccentBrush()
+        {
+            var rand = new Random();
+            var properties = typeof(AccentBrushes).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                                                  .Where(p => p.PropertyType == typeof(SolidColorBrush));
+            return (SolidColorBrush)properties.ElementAt(rand.Next(properties.Count())).GetValue(null, null);
         }
     }
 }
